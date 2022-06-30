399eaf13a6e9e878af356993335c0dce78ec0f8b
useung9 <nike4434@gmail.com>
2022-06-30 16:10:33 +0900

    reserv_update

--- src/main/java/app/spring/third/controller/ReservationController.java
+++ src/main/java/app/spring/third/controller/ReservationController.java
@@ -30,7 +30,6 @@ import app.spring.third.service.ReservationServiceImpl;
 import app.spring.third.service.RoomFileService;
 import app.spring.third.service.RoomService;
 
-
 @RequestMapping("reservation")
 @Controller
 public class ReservationController {
@@ -43,19 +42,17 @@ public class ReservationController {
 	@Autowired
 	private RoomService roomService;
 
-	
-
 	// �솃
 	@GetMapping("/")
 	public String test(HttpSession session) {
-		
+
 		return "reservation/info";
 	}
 
 	// 湲곕낯 �젙蹂� �럹�씠吏�
 	@GetMapping("info")
 	public String info() {
-		
+
 		return "reservation/info";
 	}
 
@@ -63,25 +60,23 @@ public class ReservationController {
 	@GetMapping("reservationinfo")
 	public String reservationinfo(Model model, HttpSession session) {
 		System.out.println("reservationinfo");
-		System.out.println("�꽭�뀡 媛� "+session.getAttribute("member_id"));
+		System.out.println("�꽭�뀡 媛� " + session.getAttribute("member_id"));
 		// 猷몄젙蹂� + 猷� �씠誘몄�
 		List<Map<String, Object>> roomNroomfile = roomService.roomNroomfile();
-		
+
 		model.addAttribute("roomNroomfile", roomNroomfile);
 		return "reservation/reservationinfo";
 	}
-
-
 
 	@GetMapping("reservation")
 	public String reservation(Model model, HttpSession session) {
 		// 猷몄젙蹂� + 猷� �씠誘몄�
 		List<Map<String, Object>> roomNroomfile = roomService.roomNroomfile();
-<<<<<<< HEAD
-		System.out.println("roomNroomfile"+roomNroomfile);
-=======
-		System.out.println("test"+roomNroomfile);
->>>>>>> a9c54dca934876be4f9486f4f5672fd2620a9900
+
+		System.out.println("roomNroomfile" + roomNroomfile);
+
+		System.out.println("test" + roomNroomfile);
+
 		model.addAttribute("roomNroomfile", roomNroomfile);
 
 		return "reservation/reservation";
@@ -89,37 +84,38 @@ public class ReservationController {
 
 	// �삁�빟 �떊泥�
 	@PostMapping("reservation")
-	public String reservation(Reservation reservation, RedirectAttributes rattr, HttpSession session) throws ParseException {
+	public String reservation(Reservation reservation, RedirectAttributes rattr, HttpSession session)
+			throws ParseException {
 		System.out.println("post");
-		
+
 		System.out.println(session.getAttribute("member_id"));
-		reservation.setMember_id(session.getAttribute("member_id").toString());		
-		
+		reservation.setMember_id(session.getAttribute("member_id").toString());
+
 		reservation.setRe_status("n");
-		
-	
-		  
-		  int cnt = reservationService.insert(reservation, session);
-		  // �젙�긽
-		 if(cnt == 0) { 
-			 rattr.addFlashAttribute("msg","�삁�빟�씠 �릺�뿀�뒿�땲�떎.");
-			 // �씠誘� �삁�빟�씠 �릺�뼱�엳�쓬.
-			 return "redirect:reservation";
-		 }else {
-			 rattr.addFlashAttribute("msg","�씠誘� �삁�빟�씠 �릺�뼱�엳�뒿�땲�떎.");
-			 return "reservation";
-		 }
-		
+
+		int cnt = reservationService.insert(reservation, session);
+		// �젙�긽
+		if (cnt == 0) {
+			rattr.addFlashAttribute("msg", "�삁�빟�씠 �릺�뿀�뒿�땲�떎.");
+			// �씠誘� �삁�빟�씠 �릺�뼱�엳�쓬.
+			return "redirect:reservation";
+		} else {
+			rattr.addFlashAttribute("msg", "�씠誘� �삁�빟�씠 �릺�뼱�엳�뒿�땲�떎.");
+			return "reservation";
+		}
+
 	}
-	   //諛� �긽�꽭蹂닿린瑜� �겢由��뻽�쓣 �븣 
-	   @GetMapping("roominfo")
-	   public String roominfo (int room_idx, Model model) {
-	      model.addAttribute("room_idx", roomService.selectOne(room_idx));
-	      return "reservation/roominfo";
-	   }
+
+	// 諛� �긽�꽭蹂닿린瑜� �겢由��뻽�쓣 �븣
+	@GetMapping("roominfo")
+	public String roominfo(int room_idx, Model model) {
+		model.addAttribute("room_idx", roomService.selectOne(room_idx));
+		return "reservation/roominfo";
+	}
+
 	@GetMapping("userreservation")
 	public String userreservation(HttpSession session, Model model, RedirectAttributes ratrr) {
-		System.out.println("user �븘�씠�뵒 : "+session.getAttribute("member_id"));
+		System.out.println("user �븘�씠�뵒 : " + session.getAttribute("member_id"));
 		String userid = session.getAttribute("member_id").toString();
 		List<Reservation> rlist = reservationService.getreserv(userid);
 
@@ -127,17 +123,16 @@ public class ReservationController {
 		model.addAttribute("rlist", rlist);
 		return "reservation/userreservation";
 	}
-	
-	// �삁�빟 痍⑥냼 , �뾽�뜲�씠�듃  �꽑�깮�븷�닔�엳�뒗怨�
+
+	// �삁�빟 痍⑥냼 , �뾽�뜲�씠�듃 �꽑�깮�븷�닔�엳�뒗怨�
 	@PostMapping("userreservation")
 	public String userreservation(HttpSession session, @RequestParam List<String> idx) throws Exception {
-		System.out.println("�꽭�뀡 諛쏆븘�삩 id"+session.getAttribute("member_id"));
-		String userid =session.getAttribute("member_id").toString();
+		System.out.println("�꽭�뀡 諛쏆븘�삩 id" + session.getAttribute("member_id"));
+		String userid = session.getAttribute("member_id").toString();
 		reservationService.delreservation(idx, userid);
 		return "redirect:/reservation/userreservation";
 	}
-	
-	
+
 	// �삁�빟 �뾽�뜲�씠�듃 (�쑀��蹂�)
 	@GetMapping("updatereservation")
 	public String updatereservation(@RequestParam int idx, Model model, HttpSession session) {
@@ -145,36 +140,35 @@ public class ReservationController {
 		List<Map<String, Object>> roomNroomfile = roomService.roomNroomfile();
 		model.addAttribute("roomNroomfile", roomNroomfile);
 		session.setAttribute("reservidx", idx);
-		
-		
-		
+
 		return "/reservation/updatereservation";
 	}
-	
+
 	// �닔�젙
 	@PostMapping("updatereservation")
-	public String updatereservation(RedirectAttributes rattr ,Reservation reservation,Model model, @RequestParam String roominfo, HttpSession session) {
-		String idx =session.getAttribute("reservidx").toString();
-		String userid =session.getAttribute("member_id").toString();
-		
+	public String updatereservation(RedirectAttributes rattr, Reservation reservation, Model model,
+			@RequestParam String roominfo, HttpSession session) {
+		String idx = session.getAttribute("reservidx").toString();
+		String userid = session.getAttribute("member_id").toString();
+
 //		  System.out.println("roominfo" + roominfo); System.out.println("enddate" +
 //		  reservation.getRe_enddate()); System.out.println("startdate" +
 //		  reservation.getRe_startdate());
-		 
+
 		reservation.setMember_id(userid);
 		reservation.setReservation_idx(Integer.parseInt(idx));
 		System.out.println(reservation);
 		// �빐�떦 �삁�빟嫄� �닔�젙
 		int cnt = reservationService.updatereservation(reservation, roominfo);
-		if(cnt == 1) {
-			rattr.addFlashAttribute("msg","�삁�빟 �젙蹂� �닔�젙 �셿猷�");
+		if (cnt == 1) {
+			rattr.addFlashAttribute("msg", "�삁�빟 �젙蹂� �닔�젙 �셿猷�");
 			return "redirect:/reservation/userreservation";
-		}else {
+		} else {
 			rattr.addFlashAttribute("msg", "�닔�젙以� �씠�긽 諛쒖깮 �떎�떆 �떆�룄�빐二쇱꽭�슂");
 			return "redirect:/reservation/updatereservation";
 		}
 	}
-	
+
 	/*
 	 * // �삁�빟 痍⑥냼
 	 * 
@@ -182,7 +176,7 @@ public class ReservationController {
 	 * cancelreservation(@RequestParam int idx[]) { System.out.println(idx);
 	 * reservationService.delreservation(map, userid); return ""; }
 	 */
-	
+
 // 罹섎┛�뜑 
 	@ResponseBody
 	@RequestMapping("CallCalendar")
@@ -190,59 +184,66 @@ public class ReservationController {
 
 		// 議곗씤紐⑸줉
 		List<Map<String, Object>> mlist = reservationService.getuserNreserv();
-		System.out.println("mlist"+mlist);
+		System.out.println("mlist" + mlist);
 		// 由ы꽩媛�
 		List<Map<String, Object>> resultList = new ArrayList<Map<String, Object>>();
-		
-		 // SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd"); 
-		  //�썝�븯�뒗 �뜲�씠�꽣 �룷留� 吏��젙 // �빆紐⑹텛媛� 
-		  Map<String, Object> map = new HashMap<String, Object>(); 
 
-		  // 罹섎┛�뜑 �깋�긽
-		  String color = "";
-		  // #ff7f00 二쇳솴
-		  // #0080ff �븯�뒛�깋
-		  // #33FF33 珥덈줉�깋
-		  // #7F00FF 蹂대씪�깋
-		  for (Map<String, Object> mmap : mlist) { 
-			  System.out.println("list" + mmap);
-<<<<<<< HEAD
-			  map.put("title", mmap.get("room_name")); // 방번호
-			  switch (mmap.get("room_name").toString()) {
-			  case "하늘": color = "ff7f00";
-=======
-			  map.put("title", mmap.get("ROOM_NAME")); // 諛⑸쾲�샇
-			  switch (mmap.get("ROOM_NAME").toString()) {
-			  case "�븯�뒛": color = "ff7f00";
->>>>>>> a9c54dca934876be4f9486f4f5672fd2620a9900
+		// SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		// �썝�븯�뒗 �뜲�씠�꽣 �룷留� 吏��젙 // �빆紐⑹텛媛�
+		Map<String, Object> map = new HashMap<String, Object>();
+
+		// 罹섎┛�뜑 �깋�긽
+		String color = "";
+		// #ff7f00 二쇳솴
+		// #0080ff �븯�뒛�깋
+		// #33FF33 珥덈줉�깋
+		// #7F00FF 蹂대씪�깋
+		for (Map<String, Object> mmap : mlist) {
+			System.out.println("list" + mmap);
+
+			map.put("title", mmap.get("room_name")); // 방번호
+			switch (mmap.get("room_name").toString()) {
+			case "하늘":
+				color = "ff7f00";
+
+				map.put("title", mmap.get("room_name")); // 諛⑸쾲�샇
+				switch (mmap.get("room_name").toString()) {
+				case "�븯�뒛":
+					color = "ff7f00";
+
 					break;
-			  case "�븙": color = "#0080ff";
-				  	break;  
-			  case "吏��븯": color = "#33FF33";
+				case "�븙":
+					color = "#0080ff";
 					break;
-			  case "�슜�븫": color = "#7F00FF";
+				case "吏��븯":
+					color = "#33FF33";
 					break;
-			  case "泥쒓뎅": color = "#000000";
+				case "�슜�븫":
+					color = "#7F00FF";
 					break;
-			default: color = "#000000";
-				break;
-			}
-			  map.put("color", color); 
-			  color = "";
-			  //String start = simpleDateFormat.format(mmap.get("RE_STARTDATE"));
-			  //String start =;
-			  map.put("start",  mmap.get("re_startdate"));
-			  //String end = simpleDateFormat.format(mmap.get("RE_ENDDATE"));
-			  //String end = .toString();
-			  map.put("end", mmap.get("re_enddate")+"T23:01:01");
-			  
-			  resultList.add(map); 
-			  map = new HashMap<String, Object>(); 
-			   
-			  } //
-		
-		 
+				case "泥쒓뎅":
+					color = "#000000";
+					break;
+				default:
+					color = "#000000";
+					break;
+				}
+				map.put("color", color);
+				color = "";
+				// String start = simpleDateFormat.format(mmap.get("RE_STARTDATE"));
+				// String start =;
+				map.put("start", mmap.get("re_startdate"));
+				// String end = simpleDateFormat.format(mmap.get("RE_ENDDATE"));
+				// String end = .toString();
+				map.put("end", mmap.get("re_enddate") + "T23:01:01");
+
+				resultList.add(map);
+				map = new HashMap<String, Object>();
+
+			} //
+
+
+		}
 		return resultList;
-
 	}
 }
